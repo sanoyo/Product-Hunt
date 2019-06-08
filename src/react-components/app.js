8395e8d4ce854b94afc4470e2acfda3677b03363
sanoyo <sa00380304yo@gmail.com>
2019-06-08 21:37:50 +0900

    First React App

---
+++ src/react-components/app.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+class App extends React.Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <h2>React Componet</h2>
+    );
+  }
+}
+
+ReactDOM.render(<App/>, document.getElementById('root'));
