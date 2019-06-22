5023778d90bb5a9ff9fdc1c19b2355636e430529
sanoyo <sa00380304yo@gmail.com>
2019-06-22 20:21:16 +0900

    修正

--- src/react-components/Product/ProductItem.js
+++ src/react-components/Product/ProductItem.js
@@ -3,32 +3,38 @@ import React from 'react';
 
 class ProductItem extends React.Component {
   renderUpvoteButton() {
-    <a className="upvote-button" href="#">
-      <span>
-        <i className="fa fa-sort-asc"></i>
-      </span>
-      {this.props.upvote}
-    </a>
+    return (
+      <a className="upvote-button" href="#">
+        <span>
+          <i className="fa fa-sort-asc"></i>
+        </span>
+        {this.props.upvote}
+      </a>
+    );
   }
 
   renderNewWindowIcon() {
-    <a className="product-item-link" href={this.props.link}>
-      <span>
-        <i className="fa fa-external-link"></i>
-      </span>
-    </a>
+    return (
+      <a className="product-item-link" href={this.props.link}>
+        <span>
+          <i className="fa fa-external-link"></i>
+        </span>
+      </a>
+    );
   }
 
   renderInfoSession() {
-    <section className="product-item-info">
-      <a href="#">
-        <h2>{this.props.name}</h2>
-      </a>
-      <p>{this.props.description}</p>
-      <a href="#">
-        <img className="small-avatar" src={this.props.maker.avatar}/>
-      </a>
-    </section>
+    return (
+      <section className="product-item-info">
+        <a href="#">
+          <h2>{this.props.name}</h2>
+        </a>
+        <p>{this.props.description}</p>
+        <a href="#">
+          <img className="small-avatar" src={this.props.maker.avatar}/>
+        </a>
+      </section>
+    );
   }
 
   render() {
