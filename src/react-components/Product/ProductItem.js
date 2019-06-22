c728ad21edb13ad4885193f6d3e1d0d2e1b71c68
sanoyo <sa00380304yo@gmail.com>
2019-06-22 20:09:03 +0900

    Refactor Product Item component

---
+++ src/react-components/Product/ProductItem.js
@@ -0,0 +1,46 @@
+import React from 'react';
+
+
+class ProductItem extends React.Component {
+  renderUpvoteButton() {
+    <a className="upvote-button" href="#">
+      <span>
+        <i className="fa fa-sort-asc"></i>
+      </span>
+      {this.props.upvote}
+    </a>
+  }
+
+  renderNewWindowIcon() {
+    <a className="product-item-link" href={this.props.link}>
+      <span>
+        <i className="fa fa-external-link"></i>
+      </span>
+    </a>
+  }
+
+  renderInfoSession() {
+    <section className="product-item-info">
+      <a href="#">
+        <h2>{this.props.name}</h2>
+      </a>
+      <p>{this.props.description}</p>
+      <a href="#">
+        <img className="small-avatar" src={this.props.maker.avatar}/>
+      </a>
+    </section>
+  }
+
+  render() {
+    return (
+      <li className="product-item">
+        {this.renderUpvoteButton()}
+        <img className="product-item-media" src={this.props.media} />
+        {this.renderInfoSession()}
+        {this.renderNewWindowIcon()}
+      </li>
+    );
+  }
+}
+
+export default ProductItem;
