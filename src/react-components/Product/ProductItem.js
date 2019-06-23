2c7ad8b0628f3b942a30299bd7cdc4cd0d9214e6
sanoyo <sa00380304yo@gmail.com>
2019-06-23 19:30:46 +0900

    Create Product Popup component

--- src/react-components/Product/ProductItem.js
+++ src/react-components/Product/ProductItem.js
@@ -1,7 +1,22 @@
 import React from 'react';
-
+import ProductPopup from './ProductPopup';
 
 class ProductItem extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      productPopupStatus: false
+    }
+  }
+
+  showProductPopup = () => {
+    this.setState({productPopupStatus: true});
+  };
+
+  hideProductPopup = () => {
+    this.setState({productPopupStatus: false});
+  };
+
   renderUpvoteButton() {
     return (
       <a className="upvote-button" href="#">
@@ -26,7 +41,7 @@ class ProductItem extends React.Component {
   renderInfoSession() {
     return (
       <section className="product-item-info">
-        <a href="#">
+        <a href="#" onClick={this.showProductPopup}>
           <h2>{this.props.name}</h2>
         </a>
         <p>{this.props.description}</p>
@@ -44,6 +59,7 @@ class ProductItem extends React.Component {
         <img className="product-item-media" src={this.props.media} />
         {this.renderInfoSession()}
         {this.renderNewWindowIcon()}
+        <ProductPopup status={this.state.productPopupStatus} hidePopup={this.hideProductPopup}/>
       </li>
     );
   }
