ed217d261449702a8cb9082cebb341e3483a89ec
sanoyo <sa00380304yo@gmail.com>
2019-09-28 19:55:08 +0900

    Productをfirebaseに追加できるようにする

--- src/react-components/Navbar/PostPopup.js
+++ src/react-components/Navbar/PostPopup.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import Popup from './Popup';
+import { userInfo } from 'os';
+import actions from '../../actions';
 
 class PostPopup extends React.Component {
   handlePost = () => {
+    var newProduct = {
+      name: this.refs.name.value,
+      link: this.refs.link.value,
+      descroption: this.refs.descroption.value,
+      media: this.refs.media.value,
+      upvote: 0,
+      maker: {
+        name: this.props.user.name,
+        avatar: this.props.user.avatar
+      }
+    }
 
+    actions.addProduct(newProduct)
   };
 
   render() {
@@ -15,17 +29,17 @@ class PostPopup extends React.Component {
             <tbody>
               <tr>
                 <td>Name</td>
-                <td><input placeholder="Enter product's name"/></td>
+                <td><input placeholder="Enter product's name" ref="name"/></td>
               </tr>
               <tr>
                 <td>Description</td>
-                <td><input placeholder="Enter product's description"/></td>
+                <td><input placeholder="Enter product's description" ref="description"/></td>
               </tr><tr>
                 <td>Link</td>
                 <td><input placeholder="http://www..."/></td>
               </tr><tr>
                 <td>Media</td>
-                <td><input placeholder="Pate a direct link to an image"/></td>
+                <td><input placeholder="Pate a direct link to an image" ref="media"/></td>
               </tr>
             </tbody>
           </table>
